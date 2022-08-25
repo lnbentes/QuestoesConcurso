3b09e0dca4920f00380ece431ddbce14d4d35c10
lucas bentes <lucasbentes16@gmail.com>
2022-08-25 19:01:09 -0300

    menu e questoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuestaoComponent } from './questao/questao.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "questao", pathMatch: "full"},
+  {path: "questao", component: QuestaoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
