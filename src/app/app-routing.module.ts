f03caadd479d383c853596911172298e3d236a08
lucas bentes <lucasbentes16@gmail.com>
2022-09-04 17:02:01 -0300

    finalizacao do inserir orgao

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { InserirAssuntoComponent } from './inserir/inserir-assunto/inserir-assunto.component';
 import { InserirCargoComponent } from './inserir/inserir-cargo/inserir-cargo.component';
 import { InserirDisciplinaComponent } from './inserir/inserir-disciplina/inserir-disciplina.component';
+import { InserirOrgaoComponent } from './inserir/inserir-orgao/inserir-orgao.component';
 import { InserirQuestaoComponent } from './inserir/inserir-questao/inserir-questao.component';
 import { InserirSubTopicoComponent } from './inserir/inserir-sub-topico/inserir-sub-topico.component';
 import { InserirTopicoComponent } from './inserir/inserir-topico/inserir-topico.component';
@@ -15,6 +16,7 @@ const routes: Routes = [
   {path: "inserirTopico", component: InserirTopicoComponent},
   {path: "inserirSubTopico", component: InserirSubTopicoComponent},
   {path: "inserirCargo", component: InserirCargoComponent},
+  {path: "inserirOrgao", component: InserirOrgaoComponent},
 ];
 
 @NgModule({
--- src/app/inserir/inserir-orgao/inserir-orgao.component.ts
+++ src/app/inserir/inserir-orgao/inserir-orgao.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Orgao } from 'src/app/model/Orgso';
+import { OrgaoService } from 'src/app/service/orgao.service';
 
 @Component({
   selector: 'app-inserir-orgao',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InserirOrgaoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private service: OrgaoService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public orgao: Orgao = new Orgao();
+
+  public post() {
+    this.orgao.peso = 0;
+    this.service.post(this.orgao).subscribe({next: (resp: Orgao) => {
+        this.orgao = resp;
+        alert("Enviada com sucesso!");
+        // this.router.navigate(["/inserirQuestao"]);
+      },
+      error: erro => {
+        if(erro.status == 500 || erro.status == 401 || erro.status == 400){
+          alert("Não foi possível cadastrar");
+        }
+      },
+    });
   }
 
 }
